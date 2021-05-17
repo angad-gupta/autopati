8d94c1862defe03fd188f88cd922bb0646c33811
suben-bidhee <subendra.jabegu@bidhee.com>
2021-05-17 16:36:23 +0545

    fixes

--- app/Modules/Home/Resources/views/home/detail.blade.php
+++ app/Modules/Home/Resources/views/home/detail.blade.php
@@ -5,12 +5,156 @@
 
     @include('home::home.partial.breadcrumb')
 
+    <div class="section-padding">
+        <div class="container">
+            <div class="product-full">
+                <?php
+                if($car->car_image){
+                    $imagePath = asset($car->file_full_path).'/'.$car->car_image;
+                }else{
+                    $imagePath = asset('admin/vehicle.jpeg');
+                }
+                ?>
 
-    <div class="compare-page">
+                <figure class="product-media">
+                    <img src="<?php echo e($imagePath); ?>" alt="">
+                </figure>
+
+                <div class="product-excerpt">
+                    <div class="product-views">
+                        <i class="fa fa-eye" style=""></i>  <?php echo e($car->views); ?>
+                    </div>
+
+                    <h1 class="product-title" >
+                        <?php echo e(optional($car->BrandInfo)->brand_name); ?> <?php echo e(optional($car->ModelInfo)->model_name); ?> <?php echo e(optional($car->VariantInfo)->variant_name); ?>
+                    </h1>
+
+                    <div class="product-quote">
+                        <?php echo e($car->short_quote); ?>
+                    </div>
+
+                    <div class="product-launch-date">
+                        <?php
+                        $current_date = Carbon\Carbon::now()->format('Y-m-d');
+                        ?>
+
+                        <?php if($car->expected_launch_date >= $current_date): ?>
+                        <span>Expected Launch Date :</span> <?php echo e(date('d M Y ',strtotime($car->expected_launch_date))); ?>
+                        <?php endif; ?>
+                    </div>
+
+                    <div class="product-content" style="color: gray;">
+                        <?php echo e($car->short_content); ?>
+                    </div>
+
+
+                    <div class="product-price" >
+                        Rs. <?php echo e(number_to_words($car->starting_price)); ?>
+                    </div>
+
+                    <div class="product-action-bar">
+                        <?php if($car->is_offer_available == 1): ?>
+                        <h4 class="text-center"><a href="<?php echo e(route('car.offer',$car->id)); ?>" class="btn btn-primary btn-sm ">Get Offers</a></h4>
+                        <?php endif; ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="section-padding">
+        <div class="container">
+            <div class="product-description">
+                <div class="product-information">
+                    <div class="block-title">
+                        <h3>
+                            <?php echo e(optional($car->BrandInfo)->brand_name); ?> <?php echo e(optional($car->ModelInfo)->model_name); ?> <?php echo e(optional($car->VariantInfo)->variant_name); ?>
+                        </h3>
+                    </div>
+
+                    <p><?php echo $car->description; ?>
+
+                    </p>
+                </div>
+
+                <div class="product-specification">
+                    <div class="block-title">
+                        <h3>
+                            <span>Technical Specification :</span>
+                            <!-- Product name redundant -->
+                        <?php /*echo e(optional($car->BrandInfo)->brand_name); */?><!-- <?php /*echo e(optional($car->ModelInfo)->model_name); */?> --><?php /*echo e(optional($car->VariantInfo)->variant_name); */?>
+                        </h3>
+                    </div>
+
+                    <div class="page-accordian">
+                        <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+
+                            <?php $configuration = app('\App\Modules\Configuration\Repositories\ConfigurationRepository'); ?>
+                            <?php $__currentLoopData = $car_spec; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $key => $spec_val): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+
+                            <?php
+                            $features = $configuration->findAllBySpecId($spec_val->id);
+                            $config_count = $configuration->countBySpecId($spec_val->id);
+                            ?>
+
+                            <?php if($config_count): ?>
+                            <div class="panel panel-default">
+                                <div class="panel-heading" role="tab" id="headingTwo">
+                                    <h4 class="panel-title">
+                                        <?php if($features->isNotEmpty()): ?>
+                                        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#spec-<?php echo e($spec_val->id); ?>" aria-expanded="false" aria-controls="collapseTwo">
+                                            <?php echo e($spec_val->spec_title); ?>
+
+                                        </a>
+                                        <?php endif; ?>
+                                    </h4>
+                                </div>
+                                <div id="spec-<?php echo e($spec_val->id); ?>" class="panel-collapse collapse <?php if($loop->first): ?>collapse show <?php endif; ?> role="tabpanel" aria-labelledby="headingTwo">
+                                <div class="panel-body">
+                                    <table class="table">
+                                        <tbody>
+                                        <?php $carSpecification = app('\App\Modules\Cars\Repositories\CarRepository'); ?>
+                                        <?php $__currentLoopData = $features; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $key => $spec_val): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                                        <?php
+                                        $carFeatures = $carSpecification->getFeaturesByCarId($car->id,$spec_val->id,$spec_val->id);
+                                        ?>
+
+                                        <tr>
+                                            <th scope="row"><?php echo e($spec_val->title); ?></th>
+                                            <td>
+                                                <?php if($carFeatures->isNotEmpty()): ?>
+                                                <?php $__currentLoopData = $carFeatures; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $key => $car_feat_val): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                                                <?php echo e(optional($car_feat_val->confFeatureInfo)->config_value); ?>
+
+                                                <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                                                <?php else: ?>
+                                                -
+                                                <?php endif; ?>
+                                            </td>
+                                        </tr>
+
+                                        <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                        <?php endif; ?>
+                        <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    {{--<div class="compare-page">
         <div class="container">
             <div class="row">
                 <div class="col-12">
                     <div class="compare-block">
+                        <h1>alsdkflskdjflaskdjflkasd</h1>
                         <div class="row">
                             <div class="col-12">
                                 <div style="display: flex; justify-content : space-between;">
@@ -132,7 +276,7 @@
                 </div>
             </div>
         </div>
-    </div>
+    </div>--}}
     </div>
 
     @if(sizeof($photo_feature)>0)
